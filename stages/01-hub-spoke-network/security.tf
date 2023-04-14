f4ac079eac4faa2ee109f4f1fd25fe5e77d7bd7e
Trịnh Anh Tùng <anhtung19x9@gmail.com>
2023-04-15 00:46:32 +0700

    feat: add azure firewall in hub side

--- stages/01-hub-spoke-network/security.tf
+++ stages/01-hub-spoke-network/security.tf
@@ -23,6 +23,7 @@ resource "azurerm_public_ip" "firewall" {
   location            = var.location
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
+  sku                 = "Standard"
   tags = {
     environment = "hub"
   }
